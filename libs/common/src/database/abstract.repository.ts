c3b8a8e3ba56cbc24119edb48187040d93d0cf44
RavenP <ravenp.dev@gmail.com>
2023-09-06 04:27:30 +0800

    Initial commit

---
+++ libs/common/src/database/abstract.repository.ts
@@ -0,0 +1,54 @@
+import { Logger, NotFoundException } from '@nestjs/common';
+import { AbstractDocument } from './abstract.schema';
+import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
+
+export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+  protected abstract readonly logger: Logger;
+
+  constructor(private readonly model: Model<TDocument>) {}
+
+  async find(filterQuery: FilterQuery<TDocument>) {
+    return this.model.find(filterQuery, {}, { lean: true });
+  }
+
+  async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+    const createdDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+
+    return (await createdDocument.save()).toJSON() as unknown as TDocument;
+  }
+
+  async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    const document = await this.model.findOne(filterQuery, {}, { lean: true });
+
+    if (!document) {
+      this.logger.warn('Document not found with filterQuery', filterQuery);
+      throw new NotFoundException('Document not found.');
+    }
+
+    return document as TDocument;
+  }
+
+  async findOneAndUpdate(
+    filterQuery: FilterQuery<TDocument>,
+    update: UpdateQuery<TDocument>,
+  ) {
+    const document = await this.model.findOneAndUpdate(filterQuery, update, {
+      lean: true,
+      new: true,
+    });
+
+    if (!document) {
+      this.logger.warn('Document not found with filterQuery', filterQuery);
+      throw new NotFoundException('Document not found.');
+    }
+
+    return document;
+  }
+
+  async findOneAndDelete(filterQuery: FilterQuery<TDocument>) {
+    return this.model.findOneAndDelete(filterQuery, { lean: true });
+  }
+}
---
+++ libs/common/src/interceptors/format-reponse.interceptor.ts
@@ -0,0 +1,32 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+
+interface Response<T> {
+  statusCode: number;
+  message: string;
+  data: T;
+}
+
+@Injectable()
+export class FormatResponseInterceptor<T>
+  implements NestInterceptor<T, Response<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> | Promise<Observable<Response<T>>> {
+    console.log(context.switchToHttp().getResponse());
+    return next.handle().pipe(
+      map((data) => ({
+        statusCode: context.switchToHttp().getResponse().statusCode,
+        message: data.message ?? '',
+        data: data,
+      })),
+    );
+  }
+}
